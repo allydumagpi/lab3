850264273ee01e33b07d894fef6ea04c9f2012d1
Allyza Dumagpi <119903859+allydumagpi@users.noreply.github.com>
2023-03-05 23:41:38 +0800

    Add files via upload

---
+++ ci4/app/Views/pages/index.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Allyza's Personal Portfolio</title>
+	<!---custom css link--->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<link rel="stylesheet"
+  href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+
+	<!---header--->
+	<header>
+		<a href="/lab3/ci4/public/index" style ="color:#fff; font-size: 20px;" >アセ | ルシアン</a>
+		<div class="bx bx-menu" id="menu-icon"></div>
+		
+		<ul class="navbar">
+			<li><a href="#home">Home</a></li>
+			<li><a href="#about">About Me</a></li>
+			<li><a href="#commissions">Commissions</a></li>
+			<li><a href="#portfolio">Artworks</a></li>
+			<li><a href="#contact">Contact Me</a></li>
+		</ul>
+		</header>
+		
+		<!--home--->
+		<section class="home" id="home">
+			<div class="home-text">
+			<h4>こんにちは!</h4>
+			<h1>This is azi<span>_1722</span></h1>
+			<h3>I make Fan-Arts !</h3>
+			<a href="#commissions" class="btn">Commission Me </a>
+			<h2>I'm broke T^T</h2>
+			</div>
+		</section>
+		
+		<!-- about--->
+		<section class="about" id="about">
+			<div>
+			<img src="img/hatdog.png">
+			</div>
+			
+			<div class="about-text">
+				<h2> About Me</h2>
+				<h4>Hi, I'm Allyza, <em>azi_1722</em> on Instagram and Twitter.</h4>
+				<p>20 years of age, currently studying at Asia Pacific College taking 
+				Bachelor  of  Science  in  Information  Technology  with  specialization  in  Mobile  and  Internet Technology. 
+				Things I do for fun would be binge-watching movies and series in any particular genre.
+				I started making Fan-arts wayback in 2019 (started with Haikyuu Chibi). As of now, my subject of interest in
+				art depends on whatever fandom I'm into, I also draw original characters if I'm inspired enough. Some of my 
+				work includes:</p>
+				
+				<div class="about-gri">
+					<div class="about-in">
+						<h5>Jujutsu Kaisen</h5>
+				</div>
+
+				<div class="about-in">
+					<h5>Chainsaw Man</h5>
+				</div>
+
+				<div class="about-in">
+					<h5> Genshin Impact</h5>
+				</div>
+
+				<div class="about-in">
+					<h5> OC's</h5>
+				</div>
+			</div>
+		</section>
+		
+		<!--commissions-->
+		<section class="commissions" id="commissions">
+			<div class="main-text">
+				<h2> Art Offers </h2>
+				<h4> Commission Pricing Guide </h2>
+			</div>
+			
+			<div class="commissions-content">
+				<div class="box">
+				<img src="img/1.png">
+				<h3>Chibi / Icons</h3>
+				<p> $30 (+ $15 additional character)</p>
+				</div>
+				
+				<div class="box">
+				<img src="img/2.png">
+				<h3>Bust</h3>
+				<p> $75 (+ $55 additional character)</p>
+				</div>
+				
+				<div class="box">
+				<img src="img/3.png">
+				<h3>Waist Up</h3>
+				<p> $150 (+ $100 additional character)</p>
+				</div>	
+			</div>
+		</section>
+		
+		<!--portfolio-->
+		<section class="portfolio" id="portfolio">
+			<div class="main-text">
+				<h2> Art Portfolio </h2>
+				<h4> Genshin Impact | Chainsaw Man | OC's </h4>
+			</div>
+			
+			<div class="portfolio-content">
+			<div class="row">
+				<img src="img/a1.png">
+				<div class="main-row">
+					<div class="row-text">
+						<h6>Genshin Impact</h6>
+					</div>
+					<div class="row-icon">
+						<i class='bx bx-heart'></i>
+					</div>
+				</div>
+				<h3>Raiden Ei</h3>
+			</div>
+
+			<div class="row">
+				<img src="img/a2.png">
+				<div class="main-row">
+					<div class="row-text">
+						<h6>Genshin Impact</h6>
+					</div>
+					<div class="row-icon">
+						<i class='bx bx-heart'></i>
+					</div>
+				</div>
+				<h3>Yae Miko</h3>
+			</div>
+
+			<div class="row">
+				<img src="img/a3.png">
+				<div class="main-row">
+					<div class="row-text">
+						<h6>OC</h6>
+					</div>
+					<div class="row-icon">
+						<i class='bx bx-heart'></i>
+					</div>
+				</div>
+				<h3>Raspberry</h3>
+			</div>
+
+			<div class="row">
+				<img src="img/a4.png">
+				<div class="main-row">
+					<div class="row-text">
+						<h6>OC</h6>
+					</div>
+					<div class="row-icon">
+						<i class='bx bx-heart'></i>
+					</div>
+				</div>
+				<h3>Catharsis</h3>
+			</div>
+
+			<div class="row">
+				<img src="img/a5.png">
+				<div class="main-row">
+					<div class="row-text">
+						<h6>Chainsaw Man</h6>
+					</div>
+					<div class="row-icon">
+						<i class='bx bx-heart'></i>
+					</div>
+				</div>
+				<h3>Hirofumi Yoshida</h3>
+			</div>
+
+			<div class="row">
+				<img src="img/a6.png">
+				<div class="main-row">
+					<div class="row-text">
+						<h6>Chainsaw Man</h6>
+					</div>
+					<div class="row-icon">
+						<i class='bx bx-heart'></i>
+					</div>
+				</div>
+				<h3>Asa & Yoru</h3>
+			</div>
+
+		</div>
+	</section>
+
+<!---contact--->
+	<section class="contact" id="contact">
+		<div class="contact-text">
+			<h2>Contact Me</h2>
+			<h4></h4>
+			<p>Looking forward to be working with you.</p>
+			<div class="contact-list">
+				<li><a href="#">@azi_1722 | アセ | ルシアン </a></li>
+				<li><a href="#">azimikuu@gmail.com</a></li>
+				<li><a href="#">09264555416</a></li>
+			</div>
+			<div class="contact-icons">
+				<a href="https://twitter.com/azi_1722"><i class='bx bxl-twitter' ></i></a>
+				<a href="https://www.instagram.com/azi_1722/"><i class='bx bxl-facebook' ></i></a>
+				<a href="https://www.instagram.com/azi_1722/"><i class='bx bxl-instagram' ></i></a>
+			</div>
+		</div>
+
+			<a href="/lab3/ci4/public/guest/create" class="btn btn2">Go to Commission Entry Page (❁´◡`❁) </a>
+			<br>
+			<br>
+			<br>
+              
+        </div>
+      </div>
+		
+	</section>
+	
+	
+	<!---scroll-top--->
+	<a href="#" class="top"><i class='bx bx-up-arrow-alt' ></i></a>
+
+	<script src="https://unpkg.com/scrollreveal"></script>
+
+	<!---custom js link--->
+	<script type="text/javascript" src="js/script.js"></script>
+
+	
+	</body>
+	</html>
